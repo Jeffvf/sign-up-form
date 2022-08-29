a2ece6b4dd121d1fa06d787d244bd730201dc469
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-08-29 19:50:37 -0300

    chore: improve password validation

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const confirmPass = document.querySelector("#confirm-password");
 const btn = document.querySelector("#submit-button");
 
 confirmPass.addEventListener("input", () =>{
-    if(!(pass.value === confirmPass.value)){
+    if(!(pass.value === confirmPass.value) & confirmPass.value){
         confirmPass.setCustomValidity("Passwords do not match");
-        confirmPass.reportValidity()
+        confirmPass.reportValidity();
+        confirmPass.style.borderColor = 'red';
     }
     else{
         confirmPass.setCustomValidity("");
+        confirmPass.style.borderColor = 'beige';
     }
 });
